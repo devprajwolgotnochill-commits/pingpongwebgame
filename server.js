5b5bf4f77c7084bb797bddff5ae3786e3d42bb68
devprajwolgotnochill <devprajwolgotnochill@gmail.com>
2026-03-30 14:39:59 +0545

    web pingpong game

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 8080;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//fucks all the file in src
+app.use(express.static(path.join(__dirname, "src")));
+
+app.listen(port, () =>
+  console.log($`Server running on http://localhost:{port}`),
+);
